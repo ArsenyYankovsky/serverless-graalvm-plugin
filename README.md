151db65751344e7a6efde71623c3b7909cb14fa1
ayankovsky <p44gr10@gmail.com>
2019-09-28 16:23:48 +0200

    feat(*): initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# serverless graalvm plugin
+
+Comiples your java jars into native packages making them faster and decreasing the cold start time.
---
+++ src/serverless-graalvm-plugin.ts
@@ -0,0 +1,89 @@
+import { execSync } from 'child_process'
+import * as fs from 'fs'
+import Serverless from 'serverless'
+
+export class ServerlessPlugin {
+  private serverless: Serverless
+  private hooks: { 'before:package:createDeploymentArtifacts': () => Promise<void> }
+
+  constructor(serverless: Serverless) {
+    this.serverless = serverless
+
+    this.hooks = {
+      'before:package:createDeploymentArtifacts': this.processLambdas,
+    }
+  }
+
+  public async processLambdas() {
+    this.prepareWorkingDirectory()
+
+    const lambdaNames = this.serverless.service.getAllFunctions()
+    const dockerName = `${this.serverless.service.getServiceName()}-graal`
+
+    try {
+      execSync(`docker rm ${dockerName} --force`, { stdio: 'ignore' })
+    } catch (e) {
+      // ignore all errors
+    }
+
+    lambdaNames.forEach((lambdaName) => {
+      this.processLambda(lambdaName, dockerName)
+    })
+  }
+
+  public prepareWorkingDirectory () {
+    if (!fs.existsSync('.graalvm')) {
+      fs.mkdirSync('.graalvm')
+    }
+
+    if (!fs.existsSync('reflect.json')) {
+      throw new Error('reflect.json file not found')
+    }
+
+    fs.copyFileSync('reflect.json', '.graalvm/reflect.json')
+    fs.writeFileSync('.graalvm/bootstrap', this.getBootstrapScript())
+
+    if (!fs.existsSync('resource-config.json')) {
+      fs.copyFileSync('resource-config.json', '.graalvm/resource-config.json')
+    }
+  }
+
+  public processLambda(lambdaName: string, dockerName: string) {
+    this.serverless.cli.log(`Compiling ${lambdaName} to native code`)
+
+    const packagePath = this.serverless.service.getFunction(lambdaName).package.artifact!
+
+    const clonedPackagePath = `.graalvm/${lambdaName}.jar`
+
+    fs.copyFileSync(packagePath, clonedPackagePath)
+
+    execSync(`docker run --rm --name ${dockerName} -v ${process.cwd()}/.graalvm:/.graalvm \
+        ayankovsky/serverless-graalvm-plugin-build-image:0.0.1 \
+         /bin/bash -c " \
+         native-image --enable-url-protocols=http \
+         -Djava.net.preferIPv4Stack=true \
+         -H:ReflectionConfigurationFiles=/.graalvm/reflect.json \
+         -J-Xss10m \
+         -J-Xms1g \
+         -J-Xmx14g \
+         --no-server -jar ${clonedPackagePath} && \
+         mv -f ${lambdaName} server && \
+         chmod 777 /.graalvm/bootstrap && \
+         zip -D -j ${lambdaName}.zip server /.graalvm/bootstrap && \
+         cp ${lambdaName}.zip /.graalvm"`)
+
+    this.serverless.service.getFunction(lambdaName).package = {
+      artifact: `.graalvm/${lambdaName}.zip`,
+      include: [],
+      exclude: [],
+    }
+  }
+
+  public getBootstrapScript () {
+    return `\
+#!/bin/sh
+set -euo pipefail
+./server \
+`
+  }
+}
